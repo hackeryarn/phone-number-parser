0eea6a895eb715b1d957426e332dcff1c3cbc7c6
hackeryarn <artemchernyak@gmail.com>
2019-09-01 09:15:11 -0500

    started project

--- src/PhoneNumberParser.hs
+++ src/PhoneNumberParser.hs
@@ -1,6 +1,24 @@
 module PhoneNumberParser
        ( someFunc
+       , parsePhone
+       , PhoneNumber (..)
        ) where
+
+import Text.Trifecta
+
+type NumberingPlanArea = Int
+type Exchange = Int
+type LineNumber = Int
+
+data PhoneNumber =
+  PhoneNumber NumberingPlanArea
+              Exchange
+              LineNumber
+  deriving (Eq, Show)
+
+parsePhone :: Parser PhoneNumber
+parsePhone = undefined
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import Test.Hspec
+import Text.Trifecta
+import PhoneNumberParser
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _ = Nothing
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main = hspec $ do
+  let ps = parseString
+      pspn = ps parsePhone mempty
+  describe "parsePhone" $ do
+    it "parses dash separate phone number" $ do
+      let r = pspn "123-456-7890"
+      maybeSuccess r `shouldBe` Just (PhoneNumber 123 456 7890)
